49e1ba12b5ca1d597c2f1e6d23d74309f316bc24
Roberto Soares <roberto.tech@gmail.com>
2014-09-01 19:51:18 -0300

    minor refactoring

--- lib/elixir_distributor.ex
+++ lib/elixir_distributor.ex
@@ -2,8 +2,8 @@ defmodule ElixirDistributor do
   require Record
   Record.defrecord :bitrate_playlist, bandwidth: nil, path: nil
 
-  def extract_bitrate_playlists base_path do
-    playlist_path = Path.join base_path, "playlist.m3u8"
+  def extract_bitrate_playlists basepath do
+    playlist_path = Path.join basepath, "playlist.m3u8"
 
     File.open! playlist_path, fn(pid) ->
       # Discards #EXTM3U
@@ -12,19 +12,21 @@ defmodule ElixirDistributor do
     end
   end
 
-  defp do_extract_bitrate_playlists(pid, acc) do
+  defp do_extract_bitrate_playlists pid, playlists do
     case IO.read(pid, :line) do
-      :eof -> acc
+      :eof -> playlists
       stream_inf ->
         path = IO.read(pid, :line)
-        do_extract_bitrate_playlists(pid, stream_inf, path, acc)
+        do_extract_bitrate_playlists pid, stream_inf, path, playlists
     end
   end
 
-  defp do_extract_bitrate_playlists(pid, stream_inf, path, acc)  do
+  defp do_extract_bitrate_playlists pid, stream_inf, path, playlists  do
     << "#EXT-X-STREAM-INF:PROGRAM-ID=", _program_id, ",BANDWIDTH=", bandwidth :: binary >> = stream_inf
     record = bitrate_playlist(path: String.strip(path), bandwidth: String.strip(bandwidth))
-    do_extract_bitrate_playlists(pid, [record|acc])
+    do_extract_bitrate_playlists pid, [record|playlists]
+  end
+
   end
 
 end

736803f5d59bad047454ff6ceaa1337c46d47740
Roberto Soares <roberto.tech@gmail.com>
2014-09-01 16:38:19 -0300

    added test for extract_bitrate_playlist

--- lib/elixir_distributor.ex
+++ lib/elixir_distributor.ex
@@ -1,4 +1,8 @@
 defmodule ElixirDistributor do
   require Record
   Record.defrecord :bitrate_playlist, bandwidth: nil, path: nil
+
+  def extract_bitrate_playlists path do
+    [bitrate_playlist(bandwidth: 123, path: '123')]
+  end
 end
--- test/elixir_distributor_test.exs
+++ test/elixir_distributor_test.exs
@@ -1,7 +1,11 @@
 defmodule ElixirDistributorTest do
   use ExUnit.Case
+  require ElixirDistributor
 
-  test "the truth" do
-    assert 1 + 1 == 2
+  test "extract bitrate playlists from master playlist" do
+    assert ElixirDistributor.extract_bitrate_playlists("/test/fixtures/signal_example/playlist.m3u8") ==
+      [ElixirDistributor.bitrate_playlist(bandwidth: 270336, path: "test/fixtures/signal_example/bitrate_264/playlist.m3u8"),
+      ElixirDistributor.bitrate_playlist(bandwidth: 475136, path: "test/fixtures/signal_example/bitrate_464/playlist.m3u8")]
   end
+
 end

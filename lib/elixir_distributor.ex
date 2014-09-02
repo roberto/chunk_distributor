709187ea5d7ba943b49b68f46a2669d4905b8f98
Roberto Soares <roberto.tech@gmail.com>
2014-09-01 21:13:29 -0300

    added basepath to stream playlists path

--- lib/elixir_distributor.ex
+++ lib/elixir_distributor.ex
@@ -1,30 +1,35 @@
 defmodule ElixirDistributor do
   require Record
-  Record.defrecord :bitrate_playlist, bandwidth: nil, path: nil
+  Record.defrecord :stream, bandwidth: nil, path: nil, chunks: []
 
-  def extract_bitrate_playlists basepath do
+  @doc """
+  Extract stream playlists
+  """
+  def extract_streams basepath do
     playlist_path = Path.join basepath, "playlist.m3u8"
 
     File.open! playlist_path, fn(pid) ->
       # Discards #EXTM3U
       IO.read(pid, :line)
-      do_extract_bitrate_playlists(pid, [])
+      Enum.map do_extract_streams(pid, []), fn(playlist) ->
+        stream(playlist, path: Path.join(basepath, stream(playlist, :path)))
+      end
     end
   end
 
-  defp do_extract_bitrate_playlists pid, playlists do
+  defp do_extract_streams pid, playlists do
     case IO.read(pid, :line) do
       :eof -> playlists
       stream_inf ->
         path = IO.read(pid, :line)
-        do_extract_bitrate_playlists pid, stream_inf, path, playlists
+        do_extract_streams pid, stream_inf, path, playlists
     end
   end
 
-  defp do_extract_bitrate_playlists pid, stream_inf, path, playlists  do
+  defp do_extract_streams pid, stream_inf, path, playlists  do
     << "#EXT-X-STREAM-INF:PROGRAM-ID=", _program_id, ",BANDWIDTH=", bandwidth :: binary >> = stream_inf
-    record = bitrate_playlist(path: String.strip(path), bandwidth: String.strip(bandwidth))
-    do_extract_bitrate_playlists pid, [record|playlists]
+    record = stream(path: String.strip(path), bandwidth: String.strip(bandwidth))
+    do_extract_streams pid, [record|playlists]
   end
 
   def extract_chunks bitrate_playlist do
--- test/elixir_distributor_test.exs
+++ test/elixir_distributor_test.exs
@@ -2,10 +2,10 @@ defmodule ElixirDistributorTest do
   use ExUnit.Case
   require ElixirDistributor
 
-  test "extract bitrate playlists from master playlist" do
-    assert ElixirDistributor.extract_bitrate_playlists("test/fixtures/signal_example/") ==
-      [ElixirDistributor.bitrate_playlist(bandwidth: "270336", path: "stream_264/playlist.m3u8"),
-        ElixirDistributor.bitrate_playlist(bandwidth: "475136", path: "stream_464/playlist.m3u8")]
+  test "extract streams from master playlist" do
+    assert ElixirDistributor.extract_streams("test/fixtures/signal_example/") ==
+      [ElixirDistributor.stream(bandwidth: "270336", path: "test/fixtures/signal_example/stream_264/playlist.m3u8", chunks: []),
+        ElixirDistributor.stream(bandwidth: "475136", path: "test/fixtures/signal_example/stream_464/playlist.m3u8", chunks: [])]
   end
 
   test "extract chunk files from bitrate playlist" do

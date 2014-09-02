664b4d12b7f39cffb9e4d5abc4079d144511c73b
Roberto Soares <roberto.tech@gmail.com>
2014-09-02 00:24:23 -0300

    refactoring tests with module attributes

--- test/elixir_distributor_test.exs
+++ test/elixir_distributor_test.exs
@@ -2,27 +2,32 @@ defmodule ElixirDistributorTest do
   use ExUnit.Case
   require ElixirDistributor
 
+  @stream_264 ElixirDistributor.stream(bandwidth: "270336", path: "test/fixtures/signal_example/stream_264/playlist.m3u8", chunks: [])
+  @stream_464 ElixirDistributor.stream(bandwidth: "475136", path: "test/fixtures/signal_example/stream_464/playlist.m3u8", chunks: [])
+
+
   test "extract streams from master playlist" do
     assert ElixirDistributor.extract_streams("test/fixtures/signal_example/") ==
-      [ElixirDistributor.stream(bandwidth: "270336", path: "test/fixtures/signal_example/stream_264/playlist.m3u8", chunks: []),
-        ElixirDistributor.stream(bandwidth: "475136", path: "test/fixtures/signal_example/stream_464/playlist.m3u8", chunks: [])]
+      [@stream_264, @stream_464]
   end
 
-  test "extract chunk files from bitrate playlist" do
-    bitrate_playlists = [ElixirDistributor.stream(bandwidth: "270336", path: "test/fixtures/signal_example/stream_264/playlist.m3u8", chunks: [])] 
-                        |> ElixirDistributor.extract_chunks
-                        |> List.first
+  @expected_chunks [
+    "stream_1406047228241_1406047228241_876.ts",
+    "stream_1406047228241_1406047228241_877.ts",
+    "stream_1406047228241_1406047228241_878.ts",
+    "stream_1406047228241_1406047228241_879.ts",
+    "stream_1406047228241_1406047228241_880.ts",
+    "stream_1406047228241_1406047228241_881.ts",
+    "stream_1406047228241_1406047228241_882.ts",
+    "stream_1406047228241_1406047228241_883.ts"
+  ]
 
-    assert ElixirDistributor.stream(bitrate_playlists, :chunks) == [
-      "stream_1406047228241_1406047228241_876.ts",
-      "stream_1406047228241_1406047228241_877.ts",
-      "stream_1406047228241_1406047228241_878.ts",
-      "stream_1406047228241_1406047228241_879.ts",
-      "stream_1406047228241_1406047228241_880.ts",
-      "stream_1406047228241_1406047228241_881.ts",
-      "stream_1406047228241_1406047228241_882.ts",
-      "stream_1406047228241_1406047228241_883.ts"
-    ]
+  test "extract chunk files from bitrate playlist" do
+    stream = [@stream_264] 
+              |> ElixirDistributor.extract_chunks
+              |> List.first
+
+    assert ElixirDistributor.stream(stream, :chunks) == @expected_chunks
   end
 
 end
